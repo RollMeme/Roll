cfe2a9d126580f1c42f65c6951927bdf6afa3c86
RollMeme <150623514+RollMeme@users.noreply.github.com>
2023-11-12 21:32:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Remplacez 'http://localhost:3000' par l'URL de votre backend déployé
+const response = await fetch('https://rollmeme.github.io/Roll/', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({}),
+});
 
