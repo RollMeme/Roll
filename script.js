5b20e0eaf332c2ec68a0f2599ae0197ddd58c436
RollMeme <150623514+RollMeme@users.noreply.github.com>
2023-11-12 21:40:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-// Remplacez 'http://localhost:3000' par l'URL de votre backend déployé
-const response = await fetch('https://rollmeme.github.io/Roll/', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({}),
-});
+async function spin() {
+    const response = await fetch('https://rollmeme.github.io/Roll/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // Ajoutez le header Discord-Username si vous le souhaitez
+            // 'Discord-Username': 'VotreNomDiscord',
+        },
+        body: JSON.stringify({}),
+    });
 
+    const result = await response.json();
+    
+    if (result.error) {
+        alert(result.error);
+    } else {
+        document.getElementById("result").innerText = `You got: ${result.rarity}`;
+    }
+}
